143da7e56d60639a83162d987f21740f8643b4d2
MatheusRFC <matheusr220998@gmail.com>
2020-11-22 20:18:45 -0300

    The function to hide errors is working properly.

--- Principal/JS/index.js
+++ Principal/JS/index.js
@@ -1,4 +1,58 @@
-function errorFields(fieldname)
+function errorFields_hide(fieldname)
+{
+    if(fieldname === "firstname")
+    {
+        document.getElementById("text_error_name").style.display = "none";
+    }
+
+    if(fieldname === "lastname")
+    {
+        document.getElementById("text_error_last_name").style.display = "none";
+    }
+
+    if(fieldname === "street")
+    {
+        document.getElementById("street_error").style.display = "none";
+    }
+
+    if(fieldname === "housenumber")
+    {
+        document.getElementById("house_number_error").style.display = "none";
+    }
+
+    if(fieldname === "district")
+    {
+        document.getElementById("district_error").style.display = "none";
+    }
+
+    if(fieldname === "city")
+    {
+        document.getElementById("city_error").style.display = "none";
+    }
+
+    if(fieldname === "dateofbirth")
+    {
+        document.getElementById("date_error").style.display = "none";
+    }
+
+    if(fieldname === "email")
+    {
+        document.getElementById("email_error").style.display = "none";
+    }
+
+    if(fieldname === "password")
+    {
+        document.getElementById("password_error").style.display = "none";
+    }
+
+    if(fieldname === "passwordconfirm")
+    {
+        document.getElementById("password_confirm_error").style.display = "none";
+    }
+
+}
+
+function errorFields_show(fieldname)
 {
     if(fieldname === "firstname")
     {
@@ -50,7 +104,6 @@ function errorFields(fieldname)
         document.getElementById("password_confirm_error").style.display = "block";
     }
 
-
 }
 
 function checkFields()
@@ -92,11 +145,12 @@ function checkTextField(fieldname)
     if(document.getElementById(fieldname).value.length < 2)
         {
             document.getElementById(fieldname).style.borderColor = "#F00";
-            errorFields(fieldname);
+            errorFields_show(fieldname);
         }
     else
         {
             document.getElementById(fieldname).style.borderColor = "#0F0";
+            errorFields_hide(fieldname);
         }
 }
 
@@ -105,11 +159,12 @@ function checkDate()
     if(document.getElementById("dateofbirth").value >= "2005-01-01" || document.getElementById("dateofbirth").value <= "1910-01-01")
         {
            document.getElementById("dateofbirth").style.borderColor = "#F00";
-            errorFields("dateofbirth");
+            errorFields_show("dateofbirth");
         }
     else
         {
             document.getElementById("dateofbirth").style.borderColor = "#0F0";
+            errorFields_hide("dateofbirth");
         }
 }
 
@@ -121,12 +176,13 @@ function checkEmail()
     if(emailcheck[0].length > 0 && (emailcheck[1] === "@gmail.com" || emailcheck[1] === "@hotmail.com" || emailcheck[1] === "@outlook.com" || emailcheck[1] === "@live.com" || emailcheck[1] === "@yahoo.com" || emailcheck[1] === "@aol.com" || emailcheck[1] === "@icloud.com" || emailcheck[1] === "@yandex.com")) // Stub, to be iterated over
         {
             document.getElementById("email").style.borderColor = "#0F0";
+            errorFields_hide("email");
         }
     
     else
         {
             document.getElementById("email").style.borderColor = "#F00";
-            errorFields("email");
+            errorFields_show("email");
         }
 }
 
@@ -137,17 +193,19 @@ function checkPassword()
         {
             document.getElementById("password").style.borderColor = "#F00";
             //document.getElementById("password").textContent = "Senha curta!";
-            errorFields("password");
+            errorFields_show("password");
         }
     
     else if(document.getElementById("password").value.length >= 8 && document.getElementById("password").value.length < 14)
         {
             document.getElementById("password").style.borderColor = "#FF0";
+            errorFields_hide("password");
         }
     
     else if(document.getElementById("password").value.length >= 14)
         {
             document.getElementById("password").style.borderColor = "#0F0";
+            errorFields_hide("password");
         }   
 }
 
@@ -157,12 +215,13 @@ function comparePasswords()
         {
             document.getElementById("passwordconfirm").style.borderColor = "#F00";
             //document.getElementById("passwordconfirm").textContent = "senhas não batem!";
-            errorFields("passwordconfirm");
+            errorFields_show("passwordconfirm");
         }
     
     else
         {
         document.getElementById("passwordconfirm").style.borderColor = "#0F0";
+            errorFields_hide("passwordconfirm");
         }
 }
 
@@ -170,8 +229,14 @@ function checkNumber()
 {
     if(document.getElementById("housenumber").value <= 0)
         {
-            document.getElementById("housenumber").value = 1;
-            errorFields("housenumber");
+            document.getElementById("housenumber").style.borderColor = "#F00";
+            errorFields_show("housenumber");
 
         }
+    else if(document.getElementById("housenumber").value >= 0)
+    {
+        document.getElementById("housenumber").style.borderColor = "#0F0";
+        errorFields_hide("housenumber");
+
+    }
 }
