c2c7e20aab5c7173d8ee97a85805b18e6ef2b11c
PauloCesarMS <genesis45.ppc@gmail.com>
2020-11-21 22:49:42 -0300

    Added more field checks, fixed some CSS styling conflicts, and streamlined all checks as well.

--- Principal/JS/index.js
+++ Principal/JS/index.js
@@ -12,29 +12,104 @@ function checkFields()
             document.getElementById("lastname").style.borderColor = "#F00";
         }
     
-    //Date field start
+    if(document.getElementById("street").textContent === "")
+        {
+            document.getElementById("street").style.borderColor = "#F00";
+        }
+    
+    if(document.getElementById("housenumber").value === "")
+        {
+            document.getElementById("housenumber").style.borderColor = "#F00";
+        }
+    
+    if(document.getElementById("district").textContent === "") 
+        {
+            document.getElementById("district").style.borderColor = "#F00";
+        }
+    
+    if(document.getElementById("city").textContent === "")
+        {
+            document.getElementById("city").style.borderColor = "#F00";
+        }
+}
+
+function checkTextField(fieldname)
+{
+    if(document.getElementById(fieldname).value !== "emerson")
+        {
+            document.getElementById(fieldname).style.borderColor = "#F00";
+        }
+    else
+        {
+            document.getElementById(fieldname).style.borderColor = "#0F0";
+        }
+    
+    alert(fieldname + " : " + document.getElementById(fieldname).value)
+}
+
+function checkDate()
+{
     if(document.getElementById("dateofbirth").value >= "2005-01-01" || document.getElementById("dateofbirth").value <= "1910-01-01")
         {
            document.getElementById("dateofbirth").style.borderColor = "#F00";
         }
-    //Date field end
-    
-    
-    if(document.getElementById("email").textContent !== "lal@gmail.com") // Stub, to be iterated over
+    else
+        {
+            document.getElementById("dateofbirth").style.borderColor = "#0F0";
+        }
+}
+
+function checkEmail()
+{
+    if(document.getElementById("email").value !== "lal@gmail.com") // Stub, to be iterated over
         {
             document.getElementById("email").style.borderColor = "#F00";
         }
     
-    if(document.getElementById("password").value.length <= 8) // Stub, to be iterated over
+    else
+        {
+            document.getElementById("email").style.borderColor = "#0F0";
+        }
+}
+
+
+function checkPassword()
+{
+    if(document.getElementById("password").value.length < 8)
         {
             document.getElementById("password").style.borderColor = "#F00";
             //document.getElementById("password").textContent = "Senha curta!";
         }
-    if(document.getElementById("passwordconfirm").value !== document.getElementById("password").value) // Stub, to be iterated over
+    
+    else if(document.getElementById("password").value.length >= 8 && document.getElementById("password").value.length < 14)
+        {
+            document.getElementById("password").style.borderColor = "#FF0";
+        }
+    
+    else if(document.getElementById("password").value.length >= 14)
+        {
+            document.getElementById("password").style.borderColor = "#0F0";
+        }   
+}
+
+function comparePasswords()
+{
+    if(document.getElementById("passwordconfirm").value !== document.getElementById("password").value)
         {
             document.getElementById("passwordconfirm").style.borderColor = "#F00";
             //document.getElementById("passwordconfirm").textContent = "senhas não batem!";
         }
     
-    alert(document.getElementById("dateofbirth").value);
+    else
+        {
+        document.getElementById("passwordconfirm").style.borderColor = "#0F0";
+        }
+}
+
+function checkNumber()
+{
+    if(document.getElementById("housenumber").value <= 0)
+        {
+            document.getElementById("housenumber").value = 1;
+        }
 }
